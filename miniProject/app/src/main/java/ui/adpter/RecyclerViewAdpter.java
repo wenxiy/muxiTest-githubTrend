fa9bda983e1dbc022ab41cbd6ba2d8cff1e830ad
wenxiy <193367189@qq.com>
2020-04-27 23:26:35 +0800

    写适配器

--- miniProject/app/src/main/java/ui/adpter/RecyclerViewAdpter.java
+++ miniProject/app/src/main/java/ui/adpter/RecyclerViewAdpter.java
@@ -1,15 +1,31 @@
 package ui.adpter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdpter extends RecyclerView.Adapter<> {
+import com.example.miniproject.R;
+
+import java.util.Objects;
+
+public class RecyclerViewAdpter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private final LayoutInflater layoutInflater;
+
+    public RecyclerViewAdpter(Context context) {
+        this.context = context;
+        layoutInflater=LayoutInflater.from(context);
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        return new recyclerviewholder(layoutInflater.inflate(R.layout.item,parent,false));
     }
 
     @Override
@@ -20,5 +36,12 @@ public class RecyclerViewAdpter extends RecyclerView.Adapter<> {
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    private class recyclerviewholder extends RecyclerView.ViewHolder {
+        public recyclerviewholder(View inflate) {
+            super(inflate);
+
+        }
     }
 }
